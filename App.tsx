580ba5760feac5760f8e5f98634a6f05a5533aff
Mwimwii <mpnyirongo@gmail.com>
2023-01-12 16:10:05 +0200

    1 file modified

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import './style.css';
 
 function usePi() {
   const [pi, setPi] = React.useState<string>('10');
+  const [loading, setLoading] = React.useState(false)
   const handlePi = React.useCallback((e) => {
     const size= Number(e.target.value)
     let inCircle = 0;
@@ -15,19 +16,27 @@ function usePi() {
         inCircle++
       }
     }
+    setLoading(true)
     setPi(4*inCircle/size)
+    setLoading(false)
   }, [setPi])
-  return [pi, handlePi]
+  return [pi, handlePi, loading]
 }
 
+
 function MonteCarlo() {
-  const [pi, handlePi] = usePi()
+  const [pi, handlePi, loading] = usePi()
 
   return(
-  <React.Fragment>
-      <input type="text" onChange={handlePi}/>
-      <p> Pi is about {pi} </p>
-  </React.Fragment>
+  <>
+      {!loading && 
+        (<>
+          <input type="text" onChange={handlePi}/>
+          <p> Pi is about {pi} </p>
+        </>)
+      }
+      {loading && (<p>Loading...</p>)
+  </>
   )
 }
 
